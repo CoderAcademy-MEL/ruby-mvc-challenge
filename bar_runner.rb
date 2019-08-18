149387f3c3bc088ca6200a5b330059c5332682c8
Harrison Malone <harrison.malone2@gmail.com>
2019-08-18 13:33:42 +1000

    initial commit

---
+++ bar_runner.rb
@@ -0,0 +1,19 @@
+controller = BarController.new
+controller.run
+
+
+
+# Check if the code runs
+# Add require_relative statements
+# Run the code
+# Use rubocop to clean up the code
+# Add a blank line between questions
+
+# Change the selection message to be 'Enter (1) to look at a menu or (2) to order a drink, (0) to quit'
+# Change the code to repeatedtly take orders or enter 0 to quit
+
+# Test what happens if someone tried to order a drink that is out of stock (change the stock levels from 20 to 2 for this)
+# Add a better message for ordering a drink that is out of stock
+
+# Beast++
+# Look at refactoring other code to be MVC eg: terminal app
